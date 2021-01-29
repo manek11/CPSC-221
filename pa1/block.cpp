f430a5118e29bfbff6c45acf4c21e363e5f0192d
manek11 <54968278+manek11@users.noreply.github.com>
2021-01-29 13:52:39 +0530

    pa1 init

---
+++ pa1/block.cpp
@@ -0,0 +1,19 @@
+#include "block.h"
+#include <cmath>
+#include <iostream>
+
+int Block::height() const {
+  /* your code here */
+}
+
+int Block::width() const {
+  /* your code here */
+}
+
+void Block::render(PNG &im, int x) const {
+  /* your code here */
+}
+
+void Block::build(PNG &im, int x, int width) {
+  /* your code here */
+}
---
+++ pa1/chain.cpp
@@ -0,0 +1,78 @@
+#include "chain.h"
+#include <cmath>
+#include <iostream>
+
+
+// PA1 functions
+
+/**
+ * Destroys the current Chain. This function should ensure that
+ * memory does not leak on destruction of a chain.
+ */
+Chain::~Chain() {
+  /* your code here */
+}
+
+/**
+ * Inserts a new node after the node pointed to by p in the
+ * chain (so p->next is the new node) and returns a pointer to
+ * the newly created node.
+ * If p is NULL, inserts a new head node to the chain.
+ * This function **SHOULD** create a new Node and increase length_.
+ *
+ * @param p = The new node should be pointed to by p->next.
+ *            If p is NULL, the new node becomes the head of the chain.
+ * @param ndata = The data to be inserted.
+ */
+Chain::Node * Chain::insertAfter(Node * p, const Block &ndata) {
+  /* your code here */
+}
+
+/**
+ * Swaps the position in the chain of the two nodes pointed to
+ * by p and q.
+ * If p or q is NULL or p==q, do nothing.
+ * Change the chain's head pointer if necessary.
+ */
+void Chain::swap(Node *p, Node *q) {
+  /* your code here */
+}
+
+/**
+ * Destroys all dynamically allocated memory associated with the
+ * current Chain class.
+ */
+void Chain::clear() {
+  /* your code here */
+}
+
+/**
+ * Makes the current object into a copy of the parameter:
+ * All member variables should have the same value as
+ * those of other, but the memory should be completely
+ * independent. This function is used in both the copy
+ * constructor and the assignment operator for Chains.
+ */
+void Chain::copy(Chain const &other) {
+  /* your code here */
+}
+
+/* Modifies the current chain: 
+ * 1) Find the node with the first (leftmost) block in the unscrambled
+ *    image and move it to the head of the chain.
+ *	This block is the one whose closest match (to the left) is the
+ *	largest.  That is, the distance (using distanceTo) to this block 
+ *	is big for all other blocks.
+ *	For each block B, find the distanceTo B from every other block
+ *	and take the minimum of these distances as B's "value".
+ *	Choose the block B with the maximum value over all blocks and
+ *	swap its node to the head of the chain.
+ *
+ * 2) Starting with the (just found) first block B, find the node with
+ *    the block that is the closest match to follow B (to the right)
+ *    among the remaining blocks, move (swap) it to follow B's node,
+ *    then repeat to unscramble the chain/image.
+ */
+void Chain::unscramble() {
+  /* your code here */
+}
