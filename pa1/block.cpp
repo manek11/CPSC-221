c81c34ea1e76a372453c085c3ed9d993ab54c3db
kushar77 <kusharora339@gmail.com>
2021-01-30 17:20:30 +0530

    Build method attempted

--- pa1/block.cpp
+++ pa1/block.cpp
@@ -3,17 +3,34 @@
 #include <iostream>
 
 int Block::height() const {
-  /* your code here */
+  return data.size();
 }
 
 int Block::width() const {
-  /* your code here */
+ return data.size();
 }
 
+/**
+   * Renders the given block onto img with its upper
+   * left corner at (x, 0). Assumes block fits on the 
+   * image.
+   */
 void Block::render(PNG &im, int x) const {
   /* your code here */
 }
 
+  /** 
+   * Creates a block that is width X img.height pixels in size
+   * by copying the pixels from (x, 0) to (x+width-1, img.height-1)
+   * in img. Assumes img is large enough to supply these pixels.
+   */
 void Block::build(PNG &im, int x, int width) {
-  /* your code here */
+  //Block b = new Block();
+  for (int i = x; i < x+width; i++)   {
+    for (unsigned int j = 0; j < im.height(); j++)
+   { HSLAPixel *pixel = im.getPixel(i, j);
+      data[i][j] = *pixel;
+   }
+ }
 }
+
